ec1fcec54b9665eb21cf4b986ddaa98efdbc6ce6
Luciano Camargo Cruz <luciano@lccruz.net>
2018-12-14 15:25:04 -0200

    Fixed not importing db when running the app

--- app.py
+++ app.py
@@ -12,14 +12,13 @@ from resources.store import StoreList
 
 
 app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "key_app_flask"
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity)
 
@@ -33,4 +32,10 @@ api.add_resource(UserRegister, '/register')
 if __name__ == "__main__":
     from db import db
     db.init_app(app)
-    app.run(port=5000, debug=True)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=5000)
