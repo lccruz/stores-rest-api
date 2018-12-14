a388bc15ec898a41b712e85df9cd790b7442e78a
Luciano Camargo Cruz <luciano@lccruz.net>
2018-12-14 14:22:47 -0200

    Added REST API

---
+++ security.py
@@ -0,0 +1,13 @@
+from resources.user import UserModel
+
+
+def authenticate(username, password):
+    user = UserModel.find_by_username(username)
+    if user and user.password == password:
+        return user
+    return None
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return UserModel.find_by_id(user_id)
